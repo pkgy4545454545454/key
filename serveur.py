d070a7d9ddde8b8c76d816407af13c574229250f
pkgy4545454545454 <baritropo54@gmail.com>
2026-02-02 00:19:06 +0100

    Change log file path to '/var/data/log.txt'

--- serveur.py
+++ serveur.py
@@ -8,7 +8,7 @@ from httpcore import URL
 ROOT_DIR = Path(__file__).parent
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-LOG_FILE = os.path.join(BASE_DIR, "log.txt")
+LOG_FILE = os.path.join(BASE_DIR, "/var/data/log.txt")
 
 
 app = Flask(__name__)
